b973d68201acd4226b35e9eea0160dae05ae9d08
Evgeniy Kats <eSunr1se@yandex.ru>
2024-11-28 09:31:26 +0500

    Добавить логику для film storage

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -1,2 +1,18 @@
-package ru.yandex.practicum.filmorate.storage.film;public interface FilmStorage {
+package ru.yandex.practicum.filmorate.storage.film;
+
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FilmStorage {
+    Optional<Film> getFilm(long id);
+
+    List<Film> getAllFilms();
+
+    boolean addFilm(Film film);
+
+    Optional<Film> updateFilm(Film film);
+
+    void removeFilm(long id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -1,2 +1,45 @@
-package ru.yandex.practicum.filmorate.storage.film;public class InMemoryFilmStorage {
+package ru.yandex.practicum.filmorate.storage.film;
+
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class InMemoryFilmStorage implements FilmStorage {
+    Map<Long, Film> filmsData;
+
+    public InMemoryFilmStorage() {
+        filmsData = new HashMap<>();
+    }
+
+    @Override
+    public Optional<Film> getFilm(long id) {
+        return Optional.of(filmsData.get(id));
+    }
+
+    @Override
+    public List<Film> getAllFilms() {
+        return new ArrayList<>(filmsData.values());
+    }
+
+    @Override
+    public boolean addFilm(Film film) {
+        if (filmsData.containsKey(film.getId())) return false;
+        filmsData.put(film.getId(), film);
+        return true;
+    }
+
+    @Override
+    public Optional<Film> updateFilm(Film film) {
+        if (!filmsData.containsKey(film.getId())) return Optional.empty();
+        return Optional.ofNullable(filmsData.put(film.getId(), film));
+    }
+
+    @Override
+    public void removeFilm(long id) {
+        filmsData.remove(id);
+    }
 }
